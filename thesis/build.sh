e13ef4367464706f913d64e7f6a79bbb06fb123c
Genti Saliu <gentisaliu@gmail.com>
2018-08-06 18:12:38 +0200

    Fixes

--- thesis/build.sh
+++ thesis/build.sh
@@ -1,20 +1,26 @@
 echo PDFLATEX 1st
 echo '##############################################'
-cd src/
-pdflatex src/main.tex | tee ../log_1.txt
+pdflatex main.tex
 echo '##############################################'
 echo BIBTEX
 echo '##############################################'
-bibtex src/main | tee ../log_2.txt
+bibtex main
 echo '##############################################'
 echo PDFLATEX 2nd
 echo '##############################################'
-pdflatex src/main.tex | tee ../log_3.txt
+pdflatex main.tex
 echo '##############################################'
 echo PDFLATEX 3rd
 echo '##############################################'
-pdflatex arc/main.tex | tee ../log_4.txt
+pdflatex main.tex
 echo '##############################################'
+rm -f *.aux
+rm -f *.log
+rm -f *.toc
+rm -f *.lof
+rm -f *.out
+rm -f *.lot
+rm -f *.blg
 echo DONE
 date
 echo '##############################################'
