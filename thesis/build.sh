cedc4f0e05cfef320e272a2460c50623335d432f
Genti Saliu <gentisaliu@gmail.com>
2018-08-07 11:16:44 +0200

    Fix build script

--- thesis/build.sh
+++ thesis/build.sh
@@ -14,13 +14,13 @@ echo PDFLATEX 3rd
 echo '##############################################'
 pdflatex -interaction=batchmode main.tex
 echo '##############################################'
-rm -f *.aux
-rm -f *.log
-rm -f *.toc
-rm -f *.lof
-rm -f *.out
-rm -f *.lot
-rm -f *.blg
+rm -f **/*.aux
+rm -f **/*.log
+rm -f **/*.toc
+rm -f **/*.lof
+rm -f **/*.out
+rm -f **/*.lot
+rm -f **/*.blg
 echo DONE
 date
 echo '##############################################'
